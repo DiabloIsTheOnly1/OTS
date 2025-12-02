d8332ec00a4e98be4901461b5f10f7a40c9cf427
kamaleiah <klmnop.558@gmail.com>
2025-12-02 15:33:43 +0800

    --

--- resources/views/overtime/select.blade.php
+++ resources/views/overtime/select.blade.php
@@ -38,7 +38,7 @@
                 <div>
                     <label class="block text-sm font-medium text-gray-700 mb-1">Department</label>
                     <select id="department_id" name="department_id" class="w-full px-3 py-2 border rounded-lg">
-                        <option value="">All Departments</option>
+                        <option value="">Select Departments</option>
                         @foreach ($departments as $dept)
                             <option value="{{ $dept->id }}">{{ $dept->department_name }}</option>
                         @endforeach
